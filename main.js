0f6ce671cb3b427c88865735c92acf6ef2bf70d1
Emi Tsukuda <gioramlevi13@gmail.com>
2019-05-16 22:28:16 -0700

    Final Version

--- main.js
+++ main.js
@@ -1,62 +1,74 @@
 
    
-   
+// create element to add event
     var gene = document.querySelector("#generator")
+
+    // adding event lisner to create meme
      gene.addEventListener('click', function(){
-         var container = document.createElement('div')
-        container.classList.add("col-md-8")
-         container.classList.add("out-box")
-        
+        // createing container to hold 3 element
+        var container = document.createElement('div')
+        container.classList.add("col-md-12")
+        container.classList.add("out-box")
+       
+        // capture user's input and setting to variable.
         var text = document.querySelector("#image-url");
         var topText = document.getElementById("top-meme");
         var bottomText = document.getElementById("bottom-meme");
-        var arr = [text, topText, bottomText];
         
         
+        // creating new HTML element on fly
         var ele = getImage(text.value)
-        ele.classList.add("innner-image")
-        
-        
         var ele1 = TextMeme(topText.value)
-        ele1.classList.add("text-top")
-        
-        
+        var deleteBtn = deletButton();
         var ele2 = TextMeme(bottomText.value)
+        
+        // adding unique style to each newly created element
+        ele1.classList.add("text-top")
         ele2.classList.add("text-bottom")
-       
+
+        // createing arrays to modify
+        var arr1 = [text, topText, bottomText];
+        var arr2 = [ele, ele1, ele2, deleteBtn]
         
+        // append all elements into container
+        for(let k of arr2){
+            container.appendChild(k);
+        }
         
-        for(let k of arr){
+        // clearing data in user's input field
+        for(let k of arr1){
             k.value = '';
         }
 
-        var deleteBtn = document.createElement("button")
-        deleteBtn.innerHTML = "delete"
-        deleteBtn.classList.add("delete-button");
-    
-
-        container.appendChild(deleteBtn);
-        container.appendChild(ele);
-        container.appendChild(ele1);
-        container.appendChild(ele2)
+        // append pre-defined parent div
         var holder = document.getElementById("holder")
         holder.appendChild(container)
+
+        // adding event to delete elements on fly
         deleteBtn.addEventListener("click", function(){
             holder.removeChild(container);
         })
      })
       
-    function TextMeme (text1){
+    // helper function for generate new element with user's input for text
+     function TextMeme (text1){
          var elem2 = document.createElement("span")
          elem2.innerHTML = text1
-        // elem2.classList.add('col-md-6')
         return elem2;
     }
 
-function getImage(url){
+    // helper functions for generate element with image
+    function getImage(url){
          var elem = document.createElement("img")
-        // elem.classList.add('col-md-8')
          elem.id = 'child1';
          elem.setAttribute('src', url);
+         elem.classList.add("innner-image")
          return elem
-}
+    }
+
+    function deletButton(){
+        var btn = document.createElement("button")
+        btn.innerHTML = "X"
+        btn.classList.add("delete-button");
+        return btn;
+    }
