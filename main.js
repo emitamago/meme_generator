597c70d5d89966f12ef466305f96e149327339af
Emi Tsukuda <gioramlevi13@gmail.com>
2019-05-16 18:52:27 -0700

    css added

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
    
    
-    var gene = document.querySelector(".generator")
+    var gene = document.querySelector("#generator")
      gene.addEventListener('click', function(){
          var container = document.createElement('div')
-         var uniq = Math.floor(Math.random()*20)
-        container.id = "result"+uniq
+        container.classList.add("col-md-4")
+         container.classList.add("out-box")
+        
         var text = document.querySelector("#image-url");
         var topText = document.getElementById("top-meme");
         var bottomText = document.getElementById("bottom-meme");
@@ -13,13 +14,24 @@
         
         
         var ele = getImage(text.value)
+        ele.classList.add("innner-image")
+        
+        
         var ele1 = TextMeme(topText.value)
+        ele1.classList.add("text-top")
+        
         var ele2 = TextMeme(bottomText.value)
+        ele2.classList.add("text-bottom")
+        
+        
         for(let k of arr){
             k.value = '';
         }
+
         var deleteBtn = document.createElement("button")
         deleteBtn.innerHTML = "Delete";
+        deleteBtn.classList.add("delete-button")
+    
 
         container.appendChild(deleteBtn);
         container.appendChild(ele);
@@ -35,7 +47,7 @@
     function TextMeme (text1){
          var elem2 = document.createElement("span")
          elem2.innerHTML = text1
-         elem2.classList.add('col-md-6')
+        //  elem2.classList.add('col-md-6')
         return elem2;
     }
 
@@ -43,6 +55,5 @@ function getImage(url){
          var elem = document.createElement("img")
          elem.id = 'child1';
          elem.setAttribute('src', url);
-         elem.classList.add('col-md-6')
          return elem
 }
